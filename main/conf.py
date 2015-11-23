7ad440dffd2cda0dd53df4ee9df1eb5e1cf5b479
jbrinkman <joe@dnncorp.com>
2015-11-23 18:53:35 -0500

    Begin customization of bootstrap theme

--- main/conf.py
+++ main/conf.py
@@ -111,7 +111,7 @@ html_theme_path = ["../common/themes"]
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-#html_logo = None
+html_logo = '../common/img/DNN_logo.png'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
@@ -336,7 +336,6 @@ html_theme_options = {
   ],
   'navbar_sidebarrel': True,
   'navbar_pagenav': False,
-  'source_link_position': "footer"
-
-
+  'source_link_position': "footer",
+  'navbar_title': ' '
 }
