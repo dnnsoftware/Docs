becdd6017ac806bbdcb29a6b1b1d8821822e076f
jbrinkman <joe@dnncorp.com>
2015-11-24 18:11:33 -0500

    Change logo

--- main/conf.py
+++ main/conf.py
@@ -129,7 +129,7 @@ html_theme_path = ["../common/themes"]
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = '../common/img/DNN_logo.png'
+html_logo = '../common/img/DNN_logo_28px.png'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
