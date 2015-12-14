1dba7a5607fb44393a6ae48054a7aadee0ba86ad
jbrinkman <joe@dnncorp.com>
2015-12-14 06:04:35 -0500

    Add gulp build tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var shell = require('gulp-shell');
+var argv = require('yargs').argv;
 
 gulp.task('build-main',
 	shell.task('make html', { cwd: './main' })
@@ -19,8 +20,24 @@ gulp.task('build-design',
 
 gulp.task('build-all', ['build-main', 'build-admin', 'build-dev', 'build-design']);
 
-gulp.task('watch', function () { 
-	gulp.watch(['./{admin,common,design,dev,main}/**/*.{rst,py,html,css,js}', '!./**/_build/**/*'], ['build-all'])	
+gulp.task('watch', function () {
+	switch (argv.proj) {
+		case 'main':
+			gulp.watch(['./{common,main}/**/*.{rst,py,html,css,js,txt}', '!./**/_build/**/*'], ['build-main'])
+			break;
+		case 'admin':
+			gulp.watch(['./{common,admin}/**/*.{rst,py,html,css,js,txt}', '!./**/_build/**/*'], ['build-admin'])
+			break;
+		case 'dev':
+			gulp.watch(['./{common,dev}/**/*.{rst,py,html,css,js,txt}', '!./**/_build/**/*'], ['build-dev'])
+			break;
+		case 'design':
+			gulp.watch(['./{common,design}/**/*.{rst,py,html,css,js,txt}', '!./**/_build/**/*'], ['build-design'])
+			break;
+		default: 
+			gulp.watch(['./{admin,common,design,dev,main}/**/*.{rst,py,html,css,js,txt}', '!./**/_build/**/*'], ['build-all'])
+	}
+
 });
 
 gulp.task('default', ['build-all']);
